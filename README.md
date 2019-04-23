f188c2269e0546b2a8d92371fec610cbe1cb6925
labuser <you@example.com>
2019-04-23 13:03:32 +0100

    basic file reading through stream

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# streaming101
+
+Uses SBT, so simply
+  sbt package
+
+Works with Java 10, scala 2.11
+
+To run, easiest to use spark submit
+
+* download spark: https://spark.apache.org/downloads.html
+* execute with: spark-submit --class Application --master local[4] target/scala-2.11/streaming101_2.11-0.1.jar
+
---
+++ src/main/scala/Application.scala
@@ -0,0 +1,73 @@
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
+
+object Application {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder
+      .appName("StructuredNetworkWordCount")
+      .getOrCreate()
+
+    import spark.implicits._
+
+
+
+    // Create DataFrame representing the stream of input lines from connection to localhost:9999
+    val lines = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 9999)
+      .load()
+
+    // lets start with a file based input,
+    // NOTE - this assumes each record is on an individual line
+    val inputFileData = spark.readStream.schema(buildSchema()).json("src/main/resources/readingfrom")
+
+    // see what schema we got
+    inputFileData.printSchema()
+
+    // check this is streaming
+    println("I HAVE A STRAMING DATA SOURCE? " + inputFileData.isStreaming)
+
+    // if file looks promising can move to Kafka later
+    // see https://spark.apache.org/docs/latest/structured-streaming-kafka-integration.html
+
+    var thing = inputFileData.writeStream.format("console").start()
+
+
+
+    thing.processAllAvailable()
+
+/*
+    // Split the lines into words
+    val words = lines.as[String].flatMap(_.split(" "))
+
+    // Generate running word count
+    val wordCounts = words.groupBy("value").count()
+
+    // Start running the query that prints the running counts to the console
+    val query = wordCounts.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+*/
+//    query.awaitTermination()
+
+  }
+
+
+
+  def buildSchema() : StructType = {
+    val schema = StructType(
+      Array(
+        StructField("name", StringType, true),
+        StructField("size", StringType, true)
+      )
+    )
+    schema
+  }
+
+}
